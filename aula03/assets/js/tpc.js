46a9ae72af08d0501ced04dfae27833dfd634611
Maravilha-Bambi <maravilhabambi83@gmail.com>
2025-08-27 09:21:08 +0100

    actual

---
+++ aula03/assets/js/tpc.js
@@ -0,0 +1,48 @@
+var v=12;
+console.log(v);
+console.log("===========================================================================");
+
+const idade= 20;
+var cidade="Luanda";
+console.log(    "2-"+"Tenho "+ idade + " anos  e moro em " + cidade);
+ 
+var n1=10;
+ var n2=10;
+ var n3=10;
+ var media;
+ media=(n1+n2+n3)/3;
+ console.log("3-"+"A media fina é : " +media);
+   
+var nome="Ana"
+ //prompt("Qual é o seu nome?");
+ document.writeln(nome + " seja benvinda");
+ document.writeln( "          ");
+
+/*
+let num1=parseFloat(prompt("Digite um numero"));
+
+
+let num2=parseFloat(prompt("Digite um numero"));
+let soma = num1+num2;
+let sub = num1-num2;
+let mult = num1*num2;
+let div = num1/num2;
+*/
+
+/*
+document.writeln("A soma é "+ soma);
+
+document.writeln("A subtração  é " + sub);
+
+document.writeln("A multiplicação é " + mult);
+
+document.writeln("A divisão é "+  div);
+*/
+
+
+let idade1=parseFloat(prompt("Digite a tua idade"));
+let ano=2025-idade1;
+
+
+document.writeln("O ano em que nasceste é : " + ano);
+
